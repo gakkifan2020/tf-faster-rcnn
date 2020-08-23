ee5b72cff390b9282eee0e0cad9ca95add7aab33
zhangwei <redmountainpass@sina.com>
2020-08-24 01:59:27 +0800

    源码解析

--- lib/roi_data_layer/layer.py
+++ lib/roi_data_layer/layer.py
@@ -27,19 +27,20 @@ class RoIDataLayer(object):
     self._num_classes = num_classes
     # Also set a random flag
     self._random = random
-    self._shuffle_roidb_inds()
+    self._shuffle_roidb_inds()      # 随机排列roidb
 
   def _shuffle_roidb_inds(self):
     """Randomly permute the training roidb."""
     # If the random flag is set, 
     # then the database is shuffled according to system time
     # Useful for the validation set
-    if self._random:
+    if self._random:      # 生成随机数种子
       st0 = np.random.get_state()
       millis = int(round(time.time() * 1000)) % 4294967295
       np.random.seed(millis)
     
-    if cfg.TRAIN.ASPECT_GROUPING:
+    if cfg.TRAIN.ASPECT_GROUPING:     # 使用纵横比对图像重排
+      # __C.TRAIN.ASPECT_GROUPING = False
       widths = np.array([r['width'] for r in self._roidb])
       heights = np.array([r['height'] for r in self._roidb])
       horz = (widths >= heights)
@@ -52,34 +53,39 @@ class RoIDataLayer(object):
       inds = np.reshape(inds, (-1, 2))
       row_perm = np.random.permutation(np.arange(inds.shape[0]))
       inds = np.reshape(inds[row_perm, :], (-1,))
-      self._perm = inds
-    else:
+      self._perm = inds     # 把roidb的索引打乱，造成的shuffle，打乱的索引存储的地方
+
+    else:     # 将np.arange(len(self._roidb))打乱
       self._perm = np.random.permutation(np.arange(len(self._roidb)))
     # Restore the random state
     if self._random:
       np.random.set_state(st0)
       
-    self._cur = 0
+    self._cur = 0     # 相当于一个指向_perm的指针，每次取走图片后，他会跟着变化
 
+  # 依次取IMS_PER_BATCH大小个roi的index
   def _get_next_minibatch_inds(self):
     """Return the roidb indices for the next minibatch."""
-    
-    if self._cur + cfg.TRAIN.IMS_PER_BATCH >= len(self._roidb):
+    # IMS_PER_BATCH应该在train_rpn里面改成了1
+    if self._cur + cfg.TRAIN.IMS_PER_BATCH >= len(self._roidb):     # 越界
       self._shuffle_roidb_inds()
 
-    db_inds = self._perm[self._cur:self._cur + cfg.TRAIN.IMS_PER_BATCH]
+    db_inds = self._perm[self._cur:self._cur + cfg.TRAIN.IMS_PER_BATCH]     # 下一次roidb的下标
+    # 相当于一个指向_perm的指针，随着取走图片的个数而变化
     self._cur += cfg.TRAIN.IMS_PER_BATCH
 
     return db_inds
 
   def _get_next_minibatch(self):
     """Return the blobs to be used for the next minibatch.
-
+        并没有搜到 cfg.TRAIN.USE_PREFETCH
     If cfg.TRAIN.USE_PREFETCH is True, then blobs will be computed in a
     separate process and made available through self._blob_queue.
     """
+    # 获取下一批图片的索引
     db_inds = self._get_next_minibatch_inds()
-    minibatch_db = [self._roidb[i] for i in db_inds]
+    # 把对应引索的图像信息（dict）取出来，放入一个列表中
+    minibatch_db = [self._roidb[i] for i in db_inds]      # 我感觉里面只有一个图像的信息'.'！
     return get_minibatch(minibatch_db, self._num_classes)
       
   def forward(self):
